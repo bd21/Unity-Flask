9a93fa9c837eb874d1ab24efe337add8b122811c
joel <Zippy123>
2017-11-12 23:33:50 -0800

    added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='unity-project-blake',
+    version='1.0',
+    packages=['./',],
+    url='github.com/bd21/unity-project',
+    license='MIT',
+    author='Blake Denniston',
+    author_email='bd21@uw.edu',
+    description='Unity Interview Project'
+)
